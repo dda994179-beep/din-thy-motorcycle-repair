1a59520000e86747a9cae703a04410c6c9efcfc0
ជាងឌីន ធី <316307659+dda994179-beep@users.noreply.github.com>
2026-09-13 22:51:13 +0700

    deploy

---
+++ script.js
@@ -0,0 +1,11 @@
+https://www.webtonative.com/preview/6aa69f8eb5d0c555666011b3const title = document.querySelector("#title");
+const counter = document.querySelector("#counter");
+const button = document.querySelector("#counter-button");
+
+title.innerText = "JavaScript";
+let count = 0;
+
+button.addEventListener("click", () => {
+  count++;
+  counter.innerText = count;
+});
